547cba47ca84aee2455deeb10aa7c840501da171
Nick <160446879+J7Wshinden@users.noreply.github.com>
2024-04-06 23:34:44 +0900

    Add files via upload

---
+++ Kadai_012/crud/models.py
@@ -0,0 +1,24 @@
+from typing import Any
+from django.db import models
+from django.urls import reverse
+
+class Category(models.Model):
+     name = models.CharField(max_length=200)
+   
+     def __str__(self):
+       return self.name
+
+
+class Product(models.Model):
+     name = models.CharField(max_length=200)###文字列の上限200
+     price = models.PositiveIntegerField()###価格の用数（フィールド）を正の数で
+     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+     img = models.ImageField(blank=True, default='noImage.png')
+     description = models.TextField(max_length=200, blank=True, null=True)
+
+     def __str__(self):
+        return self.name
+     
+     # 新規作成・編集完了時のリダイレクト先
+     def get_absolute_url(self):
+         return reverse('list')
