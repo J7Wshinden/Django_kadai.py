c862671fe1cd66ffedf28416dc036495dda66020
Nick <160446879+J7Wshinden@users.noreply.github.com>
2024-04-06 17:25:06 +0900

    Add files via upload

---
+++ Kadai_010/crud/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import Product
+
+class ProductListView(ListView):
+     model = Product
+     paginate_by = 3
+
+class ProductDetailView(DetailView):
+     model = Product
